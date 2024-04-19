8c27683a11fdb515c678061c1f65ef15fe262216
Deji Martins <dejimartins99@gmail.com>
2024-04-19 15:50:10 +0100

    environment setup

--- config/default.ts
+++ config/default.ts
@@ -1,3 +1,34 @@
-export default {
-    port: 8080
+import dotenv from 'dotenv';
+import { loadVariables } from 'apitoolz';
+
+dotenv.config();
+
+const DEPLOYMENT_MODE = {
+    DEV: 'development',
+    PROD: 'production',
+    TEST: 'test',
+};
+
+const NODE_ENV = process.env.NODE_ENV || DEPLOYMENT_MODE.DEV;
+
+const currentDeployment = {
+    isDev: NODE_ENV == DEPLOYMENT_MODE.DEV,
+    isProduction: NODE_ENV == DEPLOYMENT_MODE.PROD,
+    isTest: NODE_ENV == DEPLOYMENT_MODE.TEST,
+};
+
+const constants = loadVariables(
+    {
+        PORT: {
+            required: currentDeployment.isProduction,
+            default: () => (currentDeployment.isTest ? 0 : 5000),
+            parser: (v: any) => (currentDeployment.isTest ? 0: Number(v))
+        }
+    }
+)
+
+export const config = {
+    port: constants.PORT
 }
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import { config } from '../config/default';
 
 const app = express();
 
 app.listen(8080, () => {
-    console.log("App is running")
+    console.log("App is running on port", config.port )
 })
