ad9f35b63e4205e9c827460c9fe31c7d901b0c3b
Deji Martins <dejimartins99@gmail.com>
2024-05-23 21:47:52 +0100

    removed toJSON

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -7,7 +7,7 @@ import { CreateUserInput } from "schema/user.schema";
 export async function createUserHandler(req: Request<{}, {}, CreateUserInput['body']>, res: Response) {
     try{
         const user = await createUser(req.body);
-        return res.send(omit(user.toJSON(), "password"));
+        return res.send(omit(user, "password"));
     } catch (e: any) {
         log.error(e);
         return res.status(409).send(e.message);
